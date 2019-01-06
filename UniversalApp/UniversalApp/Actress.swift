989c3089de0db88b413885d626f55636d742cb02
oingbong <oingbong@gmail.com>
2019-01-06 17:22:19 +0900

    Refactring for simple code

--- UniversalApp/UniversalApp/Actress.swift
+++ UniversalApp/UniversalApp/Actress.swift
@@ -7,24 +7,62 @@
 
 import Foundation
 
-enum Actress: String, CustomStringConvertible {
-    case KR, JP, US, CN
-    
-    var description: String {
-        switch self {
-        case .KR: return "CheonUhui"
-        case .JP: return "AoiYu"
-        case .US: return "JenniferShraderLawrence"
-        case .CN: return "FanBingbing"
+struct Actress: CustomStringConvertible {
+    enum ActressInfo: String, CustomStringConvertible {
+        case KR, JP, US, CN
+        
+        var description: String {
+            switch self {
+            case .KR: return "CheonUhui"
+            case .JP: return "AoiYu"
+            case .US: return "JenniferShraderLawrence"
+            case .CN: return "FanBingbing"
+            }
+        }
+        
+        var birth: String {
+            switch self {
+            case .KR: return "1987-04-20"
+            case .JP: return "1985-08-17"
+            case .US: return "1990-08-15"
+            case .CN: return "1981-09-16"
+            }
         }
     }
     
-    var birth: String {
-        switch self {
-        case .KR: return "1987-04-20"
-        case .JP: return "1985-08-17"
-        case .US: return "1990-08-15"
-        case .CN: return "1981-09-16"
-        }
+    var description: String {
+        guard let actressInfo = self.actressInfo else { return "" }
+        return actressInfo.description
+    }
+    
+    var name: String? {
+        guard let actressInfo = self.actressInfo else { return nil }
+        return localizedString(forKey: "name", from: actressInfo.description)
+    }
+    
+    var birth: String? {
+        guard let actressInfo = self.actressInfo else { return nil }
+        return actressInfo.birth
+    }
+    
+    var info: String? {
+        guard let actressInfo = self.actressInfo else { return nil }
+        return localizedString(forKey: "info", from: actressInfo.description)
+    }
+    
+    var region: String? {
+        guard let actressInfo = self.actressInfo else { return nil }
+        return localizedString(forKey: "region", from: actressInfo.description)
+    }
+    
+    private var actressInfo: ActressInfo? {
+        guard let region = Locale.current.regionCode else { return nil }
+        guard let actressInfo = ActressInfo(rawValue: region) else { return nil }
+        return actressInfo
+    }
+    
+    private func localizedString(forKey key: String, from name: String) -> String {
+        let result = Bundle.main.localizedString(forKey: key, value: nil, table: name)
+        return result
     }
 }
--- UniversalApp/UniversalApp/Language.swift
+++ UniversalApp/UniversalApp/Language.swift
@@ -7,27 +7,45 @@
 
 import Foundation
 
-enum Language: String, CustomStringConvertible {
-    case en
-    case ko
-    case ja
-    case zh
-    
-    var description: String {
-        switch self {
-        case .en: return "English"
-        case .ko: return "한국어"
-        case .ja: return "日本の"
-        case .zh: return "中国"
+struct Language: CustomStringConvertible {
+    enum LanguageInfo: String, CustomStringConvertible {
+        case en
+        case ko
+        case ja
+        case zh
+        
+        var description: String {
+            switch self {
+            case .en: return "English"
+            case .ko: return "한국어"
+            case .ja: return "日本の"
+            case .zh: return "中国"
+            }
+        }
+        
+        var birthTitle: String {
+            switch self {
+            case .en: return "Birthday"
+            case .ko: return "생년월일"
+            case .ja: return "生年月日"
+            case .zh: return "出生日期"
+            }
         }
     }
     
-    var birthTitle: String {
-        switch self {
-        case .en: return "Birthday"
-        case .ko: return "생년월일"
-        case .ja: return "生年月日"
-        case .zh: return "出生日期"
-        }
+    var description: String {
+        guard let language = self.languageInfo else { return "" }
+        return language.description
+    }
+    
+    var birthTitle: String? {
+        guard let language = self.languageInfo else { return nil }
+        return language.birthTitle
+    }
+    
+    private var languageInfo: LanguageInfo? {
+        guard let language = Locale.current.languageCode else { return nil }
+        guard let info = LanguageInfo(rawValue: language) else { return nil }
+        return info
     }
 }
