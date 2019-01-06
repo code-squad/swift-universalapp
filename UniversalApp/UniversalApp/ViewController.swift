78232d6039fa8b520f8ca30d4f9986830b346e2b
oingbong <oingbong@gmail.com>
2019-01-06 16:20:20 +0900

    Add birthday & Add Alert

--- UniversalApp/UniversalApp/ViewController.swift
+++ UniversalApp/UniversalApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var languageRegionLabel: UILabel!
     @IBOutlet weak var birthButtton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
@@ -21,14 +21,14 @@ class ViewController: UIViewController {
     
     private func configure() {
         guard let (languageCode, regionCode) = locale() else { return }
-        guard let actressName = actress(from: regionCode) else { return }
+        guard let actress = actress(from: regionCode) else { return }
         guard let language = language(from: languageCode) else { return }
         
-        let name = localizedString(forKey: "name", from: actressName)
-        let info = localizedString(forKey: "info", from: actressName)
-        let region = localizedString(forKey: "region", from: actressName)
+        let name = localizedString(forKey: "name", from: actress.description)
+        let info = localizedString(forKey: "info", from: actress.description)
+        let region = localizedString(forKey: "region", from: actress.description)
         
-        imageView.image = UIImage(named: actressName)
+        imageView.image = UIImage(named: actress.description)
         nameLabel.text = name
         infoLabel.text = info
         languageRegionLabel.text = "\(language.description)-\(region)"
@@ -42,9 +42,9 @@ class ViewController: UIViewController {
         return (language, region)
     }
     
-    private func actress(from region: String) -> String? {
+    private func actress(from region: String) -> Actress? {
         guard let actress = Actress(rawValue: region) else { return nil }
-        return actress.description
+        return actress
     }
     
     private func language(from language: String) -> Language? {
@@ -55,5 +55,18 @@ class ViewController: UIViewController {
     private func localizedString(forKey key: String, from name: String) -> String {
         let result = Bundle.main.localizedString(forKey: key, value: nil, table: name)
         return result
+    }
+    
+    @IBAction func clickBirthButton(_ sender: UIButton) {
+        guard let (languageCode, regionCode) = locale() else { return }
+        guard let actress = actress(from: regionCode) else { return }
+        alert(with: actress)
+    }
+    
+    private func alert(with actress: Actress ) {
+        let alert = UIAlertController(title: nil, message: actress.birth, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
 }
