881f2cd514c896c706c97182560208e06ea03317
oingbong <oingbong@gmail.com>
2019-01-13 16:03:03 +0900

    Add LanguageInfo string file & Delete LanguageInfo enum

--- UniversalApp/UniversalApp/Language.swift
+++ UniversalApp/UniversalApp/Language.swift
@@ -8,44 +8,16 @@
 import Foundation
 
 struct Language: CustomStringConvertible {
-    enum LanguageInfo: String, CustomStringConvertible {
-        case en
-        case ko
-        case ja
-        case zh
-        
-        var description: String {
-            switch self {
-            case .en: return "English"
-            case .ko: return "한국어"
-            case .ja: return "日本の"
-            case .zh: return "中国"
-            }
-        }
-        
-        var birthTitle: String {
-            switch self {
-            case .en: return "Birthday"
-            case .ko: return "생년월일"
-            case .ja: return "生年月日"
-            case .zh: return "出生日期"
-            }
-        }
-    }
-    
     var description: String {
-        guard let language = self.languageInfo else { return "" }
-        return language.description
+        return localizedString(forKey: "description", from: "LanguageInfo")
     }
     
-    var birthTitle: String? {
-        guard let language = self.languageInfo else { return nil }
-        return language.birthTitle
+    var birthTitle: String {
+        return localizedString(forKey: "birthTitle", from: "LanguageInfo")
     }
     
-    private var languageInfo: LanguageInfo? {
-        guard let language = Locale.current.languageCode else { return nil }
-        guard let info = LanguageInfo(rawValue: language) else { return nil }
-        return info
+    private func localizedString(forKey key: String, from name: String) -> String {
+        let result = Bundle.main.localizedString(forKey: key, value: nil, table: name)
+        return result
     }
 }
