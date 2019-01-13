2b9b6eacd76035eaaaff4e952f66cc791d505203
oingbong <oingbong@gmail.com>
2019-01-13 16:06:40 +0900

    Edit constant property

--- UniversalApp/UniversalApp/Language.swift
+++ UniversalApp/UniversalApp/Language.swift
@@ -8,12 +8,16 @@
 import Foundation
 
 struct Language: CustomStringConvertible {
+    private let descriptionKey = "description"
+    private let birthTitleKey = "birthTitle"
+    private let LanguageInfoTable = "LanguageInfo"
+    
     var description: String {
-        return localizedString(forKey: "description", from: "LanguageInfo")
+        return localizedString(forKey: descriptionKey, from: LanguageInfoTable)
     }
     
     var birthTitle: String {
-        return localizedString(forKey: "birthTitle", from: "LanguageInfo")
+        return localizedString(forKey: descriptionKey, from: LanguageInfoTable)
     }
     
     private func localizedString(forKey key: String, from name: String) -> String {
